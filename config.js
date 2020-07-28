d7b492e216038418b24a270a24612d1e1a96c4f9
Christian Hein <chrishein@gmail.com>
2020-07-28 16:21:37 -0300

    update config to use blog's URL

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
   author: 'Christian Hein',
   githubUrl: 'https://github.com/chrishein/chrishein-blog',
   // replace this by the url where your website will be published
-  siteUrl: 'http://localhost:8000',
+  siteUrl: 'https://chrishein-blog.netlify.app/',
   social: {
     // leave the social media you do not want to appear as empty strings
     twitter: 'danskersen',
